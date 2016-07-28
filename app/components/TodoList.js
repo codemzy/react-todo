8a6b5caf71a2d4350e1a2d9621dd1632438b3254
Emma J <codemzy@gmail.com>
2016-07-28 10:01:04 +0000

    Added nothing to do message if no todos rendered to TodoList

--- app/components/TodoList.js
+++ app/components/TodoList.js
@@ -7,6 +7,13 @@ class TodoList extends React.Component {
     render() {
         var {todos} = this.props;
         var renderTodos = () => {
+            if (todos.length === 0) {
+                return (
+                    <div>
+                        <p>Nothing to do.</p>
+                    </div>
+                );
+            }
             return todos.map((todo) => {
                 return (
                     <Todo key={todo.id} text={todo.text} id={todo.id} completed={todo.completed} createdAt={todo.createdAt} completedAt={todo.completedAt} onToggle={this.props.onToggle} />
